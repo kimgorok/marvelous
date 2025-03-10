791d393d4a18440e9df8478f8ce69e7da3536efb
kimgorok <kjyse2@naver.com>
2025-03-11 01:57:37 +0900

    UI 구현

--- src/app/(with-searchbar)/search/page.tsx
+++ src/app/(with-searchbar)/search/page.tsx
@@ -1,17 +1,18 @@
-import ClientComponent from "@/app/components/client-component";
+import books from "@/mock/books.json";
+import BookItem from "@/components/book-item";
 
-export default async function Page({
+export default function Page({
   searchParams,
 }: {
-  searchParams: Promise<{ q: string }>;
+  searchParams: Promise<{
+    q?: string;
+  }>;
 }) {
-  const { q } = await searchParams;
   return (
     <div>
-      Search 페이지: {q}
-      <ClientComponent>
-        <></>
-      </ClientComponent>
+      {books.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
     </div>
   );
 }
