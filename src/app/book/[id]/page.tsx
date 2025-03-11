6cd7606aae97e82c38c322d52fe9c5c76121fc26
kimgorok <kjyse2@naver.com>
2025-03-11 13:56:33 +0900

    Page Caching

--- src/app/book/[id]/page.tsx
+++ src/app/book/[id]/page.tsx
@@ -1,4 +1,11 @@
+import { notFound } from "next/navigation";
 import style from "./page.module.css";
+
+// export const dynamicParams = false;
+
+export function generateStaticParams() {
+  return [{ id: "1" }, { id: "2" }, { id: "3" }];
+}
 
 export default async function Page({
   params,
@@ -6,9 +13,12 @@ export default async function Page({
   params: Promise<{ id: string | string[] }>;
 }) {
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/${params.id}`
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/${(await params).id}`
   );
   if (!response.ok) {
+    if (response.status === 404) {
+      notFound();
+    }
     return <div>오류가 발생했습니다...</div>;
   }
 
