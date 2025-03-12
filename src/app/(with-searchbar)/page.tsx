2b643bcfe127cc39e775fefa5da0700e6f60bde7
kimgorok <kjyse2@naver.com>
2025-03-12 12:14:53 +0900

    에러 핸들링

--- src/app/(with-searchbar)/page.tsx
+++ src/app/(with-searchbar)/page.tsx
@@ -3,13 +3,12 @@ import style from "./page.module.css";
 import { BookData } from "@/types";
 import { delay } from "@/util/delay";
 import { Suspense } from "react";
-import BookItemSkeleton from "@/components/skeleton/book-item-skeleton";
 import BookListSkeleton from "@/components/skeleton/book-list-skeleton";
 
 // NEXT_PUBLIC_API_SERVER_URL=http://localhost:12345
 
 async function AllBooks() {
-  await delay(1500);
+  await delay(500);
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`,
     { cache: "force-cache" }
@@ -29,7 +28,7 @@ async function AllBooks() {
 }
 
 async function RecoBooks() {
-  await delay(3000);
+  await delay(1000);
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`,
     { next: { revalidate: 3 } }
