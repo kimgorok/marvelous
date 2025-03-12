138557430f2f0c6d237bbf1476032bdafb0f67b5
kimgorok <kjyse2@naver.com>
2025-03-12 11:59:16 +0900

    스트리밍

--- src/app/(with-searchbar)/search/page.tsx
+++ src/app/(with-searchbar)/search/page.tsx
@@ -1,19 +1,13 @@
 import BookItem from "@/components/book-item";
+import BookListSkeleton from "@/components/skeleton/book-list-skeleton";
 import { BookData } from "@/types";
+import { delay } from "@/util/delay";
+import { Suspense } from "react";
 
-export default async function Page({
-  searchParams,
-}: {
-  searchParams: Promise<{
-    q?: string;
-  }>;
-}) {
+async function SearchResult({ q }: { q: string }) {
+  await delay(1500);
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${
-      (
-        await searchParams
-      ).q
-    }`,
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${q}`,
     { cache: "force-cache" }
   );
   if (!response.ok) {
@@ -27,5 +21,22 @@ export default async function Page({
         <BookItem key={book.id} {...book} />
       ))}
     </div>
+  );
+}
+
+export default function Page({
+  searchParams,
+}: {
+  searchParams: {
+    q?: string;
+  };
+}) {
+  return (
+    <Suspense
+      key={searchParams.q || ""}
+      fallback={<BookListSkeleton count={3} />}
+    >
+      <SearchResult q={searchParams.q || ""} />
+    </Suspense>
   );
 }
