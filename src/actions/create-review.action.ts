a991fd0991a3e905589252ed9e0c7962f8c9f2b1
kimgorok <kjyse2@naver.com>
2025-03-14 01:06:54 +0900

    revalidateTag << 완전 탠스택의 인밸리데이트쿼리

--- src/actions/create-review.action.ts
+++ src/actions/create-review.action.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { revalidatePath } from "next/cache";
+import { revalidateTag } from "next/cache";
 
 export async function createReviewAction(formData: FormData) {
   const bookId = formData.get("bookId")?.toString();
@@ -20,7 +20,7 @@ export async function createReviewAction(formData: FormData) {
       }
     );
     console.log(response.status);
-    revalidatePath(`/book/${bookId}`);
+    revalidateTag(`review-${bookId}`);
   } catch (err) {
     console.error(err);
     return;
